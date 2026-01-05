183480ce068415fc1cfad72ae89e29f405a97030
vishal12-eng <vishalrajukumarssm1111@gmail.com>
2026-01-05 17:32:36 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,6 @@ import { storage } from "./storage.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { isPrivateRoute } from "./seo.js";
-registerRoutes(app);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
